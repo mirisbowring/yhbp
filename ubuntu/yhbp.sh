851b2782efe543110012026f3c99d8d3d36f9c3d
arlindne <arthur.lindner@outlook.de>
2018-08-06 18:35:50 +0200

    fixed used system variable

--- ubuntu/yhbp.sh
+++ ubuntu/yhbp.sh
@@ -25,7 +25,6 @@ log_message(){
 }
 
 sudo iptables -I INPUT -p icmp --icmp-type 8 -m state  --state NEW,ESTABLISHED,RELATED -j LOG --log-level=1 --log-prefix "yhbp "
-src=""
 last_src=""
 
 tail -n0 -F /var/log/syslog | 
@@ -33,7 +32,7 @@ tail -n0 -F /var/log/syslog |
 	do		
 		if [[ $line = *"yhbp"* ]]; then
 			val=${line% DST=*}
-			$src=${val##*SRC=}
+			src=${val##*SRC=}
 			msg="You have been pinged by $(read_conf $src)"
 			if [[ $src == $last_src ]]; then
 				log_message $msg
